fa70e77eb87358247ce409600120aec87785b4b2
Yongze <yw850@outlook.com>
2020-03-31 00:19:17 +1100

    nearly complete project with enough feature

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,21 +12,69 @@ namespace CityInfo.API.Controllers
     [Route("api/[controller]")]
     public class CitiesController : ControllerBase
     {
+        private readonly ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
+        {
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
+        }
+
         [HttpGet]
         public IActionResult getCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            var cityEntities = _cityInfoRepository.GetCities();
+            var result = new List<CityWithoutPointOfInterestDto>();
+
+            foreach (var cityEntity in cityEntities)
+            {
+                result.Add(new CityWithoutPointOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description
+                });
+            }
+
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
-        public IActionResult getCity(int id)
+        public IActionResult getCity(int id, bool includePointOfInterest)
         {
-            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
-            if(cityToReturn == null)
+            var cityToReturn = _cityInfoRepository.GetCity(id, includePointOfInterest);
+            if (cityToReturn == null)
             {
                 return NotFound();
             }
-            return Ok(cityToReturn);
+
+            if (includePointOfInterest)
+            {
+                var result = new CityDto()
+                {
+                    Id = cityToReturn.Id,
+                    Name = cityToReturn.Name,
+                    Description = cityToReturn.Description
+                };
+
+                foreach (var point in cityToReturn.PointsOfInterest)
+                {
+                    result.PointsOfInterest.Add(new PointOfInterestDto()
+                    {
+                        Id = point.Id,
+                        Name = point.Name,
+                        Description = point.Description
+                    });
+                }
+                return Ok(result);
+            }
+
+            var citywithout = new CityWithoutPointOfInterestDto()
+            {
+                Id = cityToReturn.Id,
+                Name = cityToReturn.Name,
+                Description = cityToReturn.Description
+            };
+            return Ok(citywithout);
         }
     }
 }
---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,43 @@
+﻿using CityInfo.API.Context;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointOfInterest)
+        {
+            if (includePointOfInterest)
+            {
+                return _context.Cities.Include(c => c.PointsOfInterest).Where(c => c.Id == cityId).FirstOrDefault();
+            }
+            return _context.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            return _context.PointOfInterests.Where(p => p.Id == pointOfInterestId && p.CityId == cityId).FirstOrDefault();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointOfInterests(int cityId)
+        {
+            return _context.PointOfInterests.Where(p => p.Id == cityId).ToList();
+        }
+    }
+}
